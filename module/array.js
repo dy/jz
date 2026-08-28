9657038116de74e60ad16a2b02f5785655557fe3
Dmitry Iv. <df.creative@gmail.com>
2026-08-28 16:40:12 -0400

    array: extract early-exit iteration family (.some/.every/.find*) into module/array/early-exit.js

--- module/array.js
+++ module/array.js
@@ -25,6 +25,7 @@ import { REP_EDGE_REJECT, representationProgramHasBigint, representationStorageW
 import { plannedTypedStorageCtor } from '../src/compile/typed-storage-plan.js'
 import { hoistArrayValue, makeCallback, callbackArgReps, idxF64, idxArg } from './array/callback.js'
 import { arrayFromEmit } from './array/from.js'
+import { registerEarlyExit } from './array/early-exit.js'
 
 
 // Complement of {ARRAY, TYPED} in the VAL domain — the kindSet argument
@@ -1576,67 +1577,7 @@ export default (ctx) => {
     (i32.store (i32.sub (local.get $off) (i32.const 8)) (local.get $newLen))
     (local.get $out))`
 
-  // Early-exit callback iterator: init value, exit test, value on match.
-  const earlyExitMethod = ({ tag, init, test, onMatch, reverse }) => (arr, fn) => {
-    const recv = hoistArrayValue(arr)
-    const r = temp(tag)
-    const exit = `$exit${freshId(ctx)}`
-    const cb = makeCallback(fn, callbackArgReps(arr))
-    const loop = arrayLoop(recv.value, (_ptr, _len, i, item) => [
-      ['if', test(cb, i, item),
-        ['then', ['local.set', `$${r}`, onMatch(cb, i, item)], ['br', exit]]]
-    ], undefined, undefined, reverse)
-    return typed(['block', ['result', 'f64'],
-      recv.setup,
-      cb.setup,
-      ['local.set', `$${r}`, init],
-      ['block', exit, ...loop],
-      ['local.get', `$${r}`]], 'f64')
-  }
-
-  ctx.core.emit['.some'] = earlyExitMethod({
-    tag: 'sr',
-    init: ['f64.const', 0],
-    test: (cb, i, item) => truthyIR(cb.call([item, idxArg(cb, i)])),
-    onMatch: () => ['f64.const', 1],
-  })
-
-  ctx.core.emit['.every'] = earlyExitMethod({
-    tag: 'ev',
-    init: ['f64.const', 1],
-    test: (cb, i, item) => ['i32.eqz', truthyIR(cb.call([item, idxArg(cb, i)]))],
-    onMatch: () => ['f64.const', 0],
-  })
-
-  ctx.core.emit['.findIndex'] = earlyExitMethod({
-    tag: 'fi',
-    init: ['f64.const', -1],
-    test: (cb, i, item) => truthyIR(cb.call([item, idxArg(cb, i)])),
-    onMatch: (_cb, i) => ['f64.convert_i32_s', ['local.get', `$${i}`]],
-  })
-
-  ctx.core.emit['.find'] = earlyExitMethod({
-    tag: 'ff',
-    init: ['f64.reinterpret_i64', ['i64.const', NULL_NAN]],
-    test: (cb, i, item) => truthyIR(cb.call([item, idxArg(cb, i)])),
-    onMatch: (_cb, _i, item) => item,
-  })
-
-  ctx.core.emit['.findLastIndex'] = earlyExitMethod({
-    tag: 'fli',
-    init: ['f64.const', -1],
-    test: (cb, i, item) => truthyIR(cb.call([item, idxArg(cb, i)])),
-    onMatch: (_cb, i) => ['f64.convert_i32_s', ['local.get', `$${i}`]],
-    reverse: true,
-  })
-
-  ctx.core.emit['.findLast'] = earlyExitMethod({
-    tag: 'fl',
-    init: ['f64.reinterpret_i64', ['i64.const', NULL_NAN]],
-    test: (cb, i, item) => truthyIR(cb.call([item, idxArg(cb, i)])),
-    onMatch: (_cb, _i, item) => item,
-    reverse: true,
-  })
+  registerEarlyExit()
 
   // === Array methods ===
 
